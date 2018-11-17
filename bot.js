abd0b327e003b6a105ba22cdc2ba03787b836a1f
rwnwn <45011018+rwnwn@users.noreply.github.com>
2018-11-17 13:41:33 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,39 +1,38 @@
-
 const Discord = require('discord.js');
 const client = new Discord.Client();
 
 const devs = ["501902518302015501"]; //الايدي حقك
 const adminprefix = ["A"]; // البرفكس
 client.on('message', message => {
-    var argresult = message.content.split().slice(1).join(' ');
+    var argresult = message.content.split(` `).slice(1).join(' ');
       if (!devs.includes(message.author.id)) return;
 
-  if (message.content.startsWith(adminprefix + 'hygt5rrrredcwxssx')) {
+  if (message.content.startsWith(adminprefix + 'gg')) {
     client.user.setGame(argresult);
-      message.channel.send(:white_check_mark:   ${argresult})
+      message.channel.send(`**✅   ${argresult}**`)
   } else
-     if (message.content === (adminprefix + "tgrdxwdcwefvrgtbhy")) {
+     if (message.content === (adminprefix + "leave")) {
     message.guild.leave();
   } else
-  if (message.content.startsWith(adminprefix + 'tgrfedcrfgtrfdeccfrgt')) {
+  if (message.content.startsWith(adminprefix + 'gg')) {
   client.user.setActivity(argresult, {type:'WATCHING'});
-      message.channel.send(:white_check_mark:   ${argresult})
+      message.channel.send(`**✅   ${argresult}**`)
   } else
-  if (message.content.startsWith(adminprefix + 'ytgrfecfrvgtvrfcdfvrcd')) {
+  if (message.content.startsWith(adminprefix + 'gg')) {
   client.user.setActivity(argresult , {type:'LISTENING'});
-      message.channel.send(:white_check_mark:   ${argresult})
+      message.channel.send(`**✅   ${argresult}**`)
   } else
   if (message.content.startsWith(adminprefix + 'st')) {
     client.user.setGame(argresult, "https://www.twitch.tv/idk");
-      message.channel.send(:white_check_mark:)
+      message.channel.send(`**✅**`)
   }
-  if (message.content.startsWith(adminprefix + 'ybtrgvcedwsxcdwevgtrb')) {
+  if (message.content.startsWith(adminprefix + 'setggname')) {
   client.user.setUsername(argresult).then
-      message.channel.send(Changing The Name To ..${argresult} )
+      message.channel.send(`Changing The Name To ..**${argresult}** `)
 } else
-if (message.content.startsWith(adminprefix + 'ynbtrvfecdert453fedcsxd')) {
+if (message.content.startsWith(adminprefix + 'gtavatar')) {
   client.user.setAvatar(argresult);
-    message.channel.send(Changing The Avatar To :${argresult});
+    message.channel.send(`Changing The Avatar To :**${argresult}** `);
 }
 });
- client.login(process.env.BOT_TOKEN);
+ client.login(process.env.BOT_TOKEN); 
